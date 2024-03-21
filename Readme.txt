82873d0622782a820300b529b9c66ea7505d50a5
pgiweb <162114161+pgiweb@users.noreply.github.com>
2024-03-21 03:54:34 -0700

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Contact for website every type of big database to small.
+9806872953
+samratthapa2084@gmail.com
+
---
+++ script.js
@@ -0,0 +1,57 @@
+// function menTest() {
+//     const men_one = select("#men_card1");
+//     const men_two = select("#men_card2");
+//     const men_three = select("#men_card3");
+//     const men_four = select("#men_card4");
+//     const men_five = select("#men_card5");
+//     const men_six = select("#men_card6");
+//     const test_one= select("#faq1"); 
+//     const test_two= select("#faq2"); 
+//     const test_three= select("#faq3"); 
+//     const test_four= select("#faq4"); 
+//     const test_five= select("#faq5"); 
+//     const test_six= select("#faq6"); 
+//     men_one.addEventListener("click", function(){
+//         test_two.classList.toggle("hide");
+//         test_three.classList.toggle("hide");
+//         test_four.classList.toggle("hide");
+//         test_five.classList.toggle("hide");
+//         test_six.classList.toggle("hide");
+//     });
+//     men_two.addEventListener("click", function(){
+//         test_one.classList.toggle("hide");
+//         test_three.classList.toggle("hide");
+//         test_four.classList.toggle("hide");
+//         test_five.classList.toggle("hide");
+//         test_six.classList.toggle("hide");
+//     });
+// }
+
+// function select(selector) {
+//     return document.querySelector(selector);
+// }
+
+// menTest();
+
+
+// .custom-cursor:hover {
+//     cursor: pointer; 
+//     color: red; 
+// }
+{/* <script>
+function confirmRedirect() {
+var confirmation = window.confirm("Are you sure you want to proceed?");
+if (confirmation) {
+    
+    window.location.href = 'index.html';
+} 
+}
+</script> */}
+
+
+{/* <script>
+function redirectToPage() {
+    // Replace 'destination.html' with the actual URL of the page you want to redirect to
+    window.location.href = 'loggedpage.html';
+}
+</script> */}
